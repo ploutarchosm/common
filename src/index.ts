8f0d65b3bf24f1c3755ed2ad06bd748559b289ea
Ploutarchos Michaelides <info@ploutarchosm.com>
2025-07-07 15:08:04 +0300

    export ts files

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,25 @@ export * from './lib/services/namespace-manager.service';
 // INTERFACES
 export * from './lib/interfaces/namespace-configuration.interface';
 export * from './lib/interfaces/namespace-module-options.interface';
+export * from './lib/interfaces/permission.interface';
 
 // CONST
 export * from './lib/constants/namespace-config-key';
 export * from './lib/constants/localization.config-key';
+export * from './lib/constants/security-api-token-header-key';
+export * from './lib/constants/security-meta-data-key';
 
 // MODULES
 export * from './lib/modules/namespace.module';
 
 // HELPERS
 export * from './lib/helpers/throw-error-stack';
+export * from './lib/helpers/has-permission';
+
+// DECORATORS
+export * from './lib/decorators/permissions.decorator';
+
+// GUARDS
+export * from './lib/guards/permissions.guard';
+export * from './lib/guards/authorized.guard';
+
